d5e76ed4e82658114c46705e9a9e63b1f4666749
Mateusz Palyz <m.palyz@pilot.co>
2015-05-27 22:53:59 +0200

    add send message handler and current coversation helper

--- app/main/controllers/main_controller.rb
+++ app/main/controllers/main_controller.rb
@@ -9,6 +9,18 @@ module Main
 
     def select_conversation(user)
       params._user_id = user._id
+      page._new_message = ''
+    end
+
+    def send_message
+      unless page._new_message.strip.empty?
+        _messages << { sender_id: Volt.current_user._id, receiver_id: params._user_id, text: page._new_message }
+        page._new_message = ''
+      end
+    end
+
+    def current_conversation
+      _messages.find({ "$or" => [{ sender_id: Volt.current_user._id, receiver_id: params._user_id }, { sender_id: params._user_id, receiver_id: Volt.current_user._id }] })
     end
 
     private
